b26ef08d032b0d1b6831ef2eb8918bc9eda00660
caiym <caiym@tuya.com>
2021-07-01 01:04:40 +0800

    1

---
+++ allpay-web/src/main/java/com/cym/allpay/web/AllPayWebApplication.java
@@ -0,0 +1,13 @@
+package com.cym.allpay.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllPayWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllPayWebApplication.class, args);
+    }
+
+}
